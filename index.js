10ba1aaa7574c2b3a82781fd20c4c40f969feda2
Juan Toro <juand1360@gmail.com>
2023-12-04 22:19:05 -0500

    Se crea conexion con base de datos y parametrizacion de los datos del f29c

--- index.js
+++ index.js
@@ -1,41 +1,81 @@
-const net = require('net');
+import * as net from "net";
+import admin from 'firebase-admin';
+import {COMMANDS} from './src/constants/commands.constants.js';
 
+// Obtener el key del service account de firebase
+import serviceAccount from "./config/key.json" assert { type: "json" };
+
+// URL de la base de datos de firebase
+const DATABASE_URL =
+  "https://csi2-a53ef-default-rtdb.asia-southeast1.firebasedatabase.app/";
+
+
+//inicializar la conexión usando el key y la url de la base datos
+const firebaseApp = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: DATABASE_URL,
+});
+
+// Obtener una referencia a la base de datos
+const db = admin.database();
+// Esta referencia es el sub-nivel en el que se va a agregar información de logs
+const ref = db.ref("/logs");
+
+// Inicializar el servidor
 const server = net.createServer((socket) => {
-  socket.on('data', (data) => {
+  // Cuando se reciben datos
+  socket.on("data", (data) => {
     const message = data.toString();
     console.log(`Received data: ${message}`);
 
     // Parse the received message
-    const [manufacturer, deviceId, contentLength, content] = parseMessage(message);
+    const [manufacturer, deviceId, contentLength, content] =
+      parseMessage(message);
 
-/*     console.log(`Manufacturer: ${manufacturer}`);
-    console.log(`Device ID: ${deviceId}`);
-    console.log(`Content Length: ${contentLength}`);
-    console.log(`Content: ${content}`); */
+    // Si el mensaje recibido contiene la propiedad content y un deviceId
+    if (content && deviceId) {
+      const command = COMMANDS[content[0]];
+    
 
-    // Process the message or send a response as needed
-    // ...
+      // Si el command que se recibió requiere logging
+      if (command && command.log) {
+        console.log(`Command ${content[0]}, requires logging`);
+        const logRef = ref.child("test");
+        logRef.push({
+          data: content,
+          deviceId: deviceId,
+        });
+      }
 
-    // Example: Send a response back to the client
-    const response = `[3G*4305000425*0002*LK]`; // Example response
-    socket.write(response);
+      // Si el command que se recibió requiere enviar una respuesta devuelta al cliente
+      if (command && command.responseRequired) {
+        const response = `[3G*${deviceId}*0002*LK]`;
+        socket.write(response);
+        console.log(`Command ${content[0]}, requires response`);
+      }
+
+      socket.end();
+
+    }  
+    socket.end(); 
   });
 
-  socket.on('end', () => {
-    console.log('Client disconnected');
+  // Si la conección se cierra
+  socket.on("end", () => {
+    console.log("Client disconnected");
   });
 });
 
 const PORT = 4000;
-const ADDRESS = '0.0.0.0';
+const ADDRESS = "0.0.0.0";
 server.listen(PORT, ADDRESS, () => {
   console.log(`Server listening on ${ADDRESS}:${PORT}`);
 });
 
 function parseMessage(message) {
-  const [manufacturer, deviceId, contentLength, content] = message
+  let [manufacturer, deviceId, contentLength, content] = message
     .slice(1, -1) // Remove brackets
-    .split('*');
-
+    .split("*");
+  content = content.split(",");
   return [manufacturer, deviceId, contentLength, content];
 }
---
+++ src/constants/commands.constants.js
@@ -0,0 +1,14 @@
+export const COMMANDS = {
+  LK: {
+    responseRequired: true,
+    log: true,
+  },
+  TEST: {
+    responseRequired: false,
+    log: true,
+  },
+  TEST2: {
+    responseRequired: false,
+    log: false,
+  },
+};
