878d3d0fc0aaf5a487c0f4339aadccef413fbbbc
Juan Toro <juand1360@gmail.com>
2023-12-08 15:10:39 -0500

    se cambia el archivo index por el codigo basico de tcp

--- index.js
+++ index.js
@@ -1,108 +1,42 @@
-import { DateTime } from "luxon";
-import * as net from "net";
-import * as dotenv from "dotenv";
-import { MongoClient, ServerApiVersion } from "mongodb";
-import { COMMANDS } from "./src/constants/commands.constants.js";
-import {
-  LOCATION,
-  LOCATION_COMMANDS,
-} from "./src/constants/location.constants.js";
-dotenv.config();
-const uri = process.env.MONGO_URI;
-const dbName = process.env.DB_NAME;
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+import * as net from 'net';
 
-await client.connect();
-
-
-// Inicializar el servidor
 const server = net.createServer((socket) => {
-  socket.on("connect", (data) => {
-    console.log(`Connected:`, data)
-  })
-
-
-  // Cuando se reciben datos
-  socket.on("data", (data) => {
+  socket.on('data', (data) => {
     const message = data.toString();
     console.log(`Received data: ${message}`);
 
     // Parse the received message
-    const [manufacturer, deviceId, contentLength, content, parsedContent] =
-      parseMessage(message);
+    const [manufacturer, deviceId, contentLength, content] = parseMessage(message);
 
-    // Si el mensaje recibido contiene la propiedad content y un deviceId
-    if (content && deviceId) {
-      const command = COMMANDS[content[0]];
+    console.log(`Manufacturer: ${manufacturer}`);
+    console.log(`Device ID: ${deviceId}`);
+    console.log(`Content Length: ${contentLength}`);
+    console.log(`Content: ${content}`);
 
-      // Si el command que se recibió requiere logging
-      if (command && command.log) {
-        console.log(`Command ${content[0]}, requires logging`);
+    // Process the message or send a response as needed
+    // ...
 
-        const logData = {
-          date: DateTime.now().setZone("America/Bogota").toISO(),
-          data: content,
-          deviceId: deviceId,
-          parsedData: parsedContent,
-        };
-
-        const collection = client.db(dbName).collection(command.collection);
-
-        collection.insertOne(logData);
-
-
-      }
-
-      // Si el command que se recibió requiere enviar una respuesta devuelta al cliente
-      if (command && command.responseRequired) {
-        const response = `[3G*${deviceId}*0002*LK]`;
-        socket.write(response);
-        console.log(`Command ${content[0]}, requires response: ${response}`);
-        return;
-      }
-
-      socket.end();
-      return;
-    }
-    socket.end();
+    // Example: Send a response back to the client
+    const response = `[3G*4305000452*0002*LK]`; // Example response
+    socket.write(response);
   });
 
-  // Si la conexión se cierra
-  socket.on("end", () => {
-    console.log("Client disconnected");
+  socket.on('end', () => {
+    console.log('Client disconnected');
   });
 });
 
-const PORT = process.env.PORT;
-const ADDRESS = process.env.ADDRESS;
+const PORT = 4000;
+const ADDRESS = '0.0.0.0';
 server.listen(PORT, ADDRESS, () => {
   console.log(`Server listening on ${ADDRESS}:${PORT}`);
 });
 
 function parseMessage(message) {
-  let [manufacturer, deviceId, contentLength, content] = message
+  const [manufacturer, deviceId, contentLength, content] = message
     .slice(1, -1) // Remove brackets
-    .split("*");
-  let parsedContent = {};
-  content = content.split(",");
-  // Verifica si el contenido existe y si el primer elemento está incluido en LOCATION_COMMANDS(comandos que contienen ubicación).
-  if (content && content[0] && LOCATION_COMMANDS.includes(content[0])) {
-    console.log(content[0]);
-    // Crea un nuevo objeto parsedContent asignando a cada propiedad un nombre (location) y un valor  correspondiente extraído de content.
-    parsedContent = LOCATION.reduce((object, value, index) => {
-      const key = value;
-      object[key] = content[index];
-      return object;
-    }, {});
-    // Imprime parsedContent en la consola
-    console.log(parsedContent);
-  }
-  return [manufacturer, deviceId, contentLength, content, parsedContent];
+    .split('*');
+
+  return [manufacturer, deviceId, contentLength, content];
 }
+
